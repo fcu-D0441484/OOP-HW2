00f0e13b3c7535604320170d468f160d637bf708
user <user@FC02>
2016-10-14 09:53:21 +0800

    新增Break、Continue的Java檔,修改StringComparator

--- src/lab3/StringComparator.java
+++ src/lab3/StringComparator.java
@@ -6,14 +6,22 @@ public class StringComparator {
 		// TODO Auto-generated method stub
 		String one;
 		String two;
-		System.out.println("Enter string 1:");
-		one=new Scanner(System.in).nextLine();
-		System.out.println("Enter string 2:");
-		two=new Scanner(System.in).nextLine();
-		if(one.equalsIgnoreCase(two))
-		System.out.println("The two strings are the same.");
-		else
-		System.out.println("The two strings are not the same.");
-	}
+		while(true)
+		{
+			System.out.println("Enter string 1:");
+			one=new Scanner(System.in).nextLine();
+			System.out.println("Enter string 2:");
+			two=new Scanner(System.in).nextLine();
+			if(one.equalsIgnoreCase(two))
+			{
+				System.out.println("The two strings are the same.");
+				break;
+			}
+			else
+			System.out.println("The two strings are not the same.");
+	
+		}
+	
 
+}
 }
